401cbb0d9fd90a6000ea58f6606acc4a5ccd43db
yashhsoni <128360447+yashhsoni@users.noreply.github.com>
2025-04-05 15:16:51 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "48d76acf-b951-48f0-bd29-6e39414dfcf3"
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = "rg-jenkins"
+  location = var.location
+}
+
+resource "azurerm_service_plan" "this" {
+  name                = "appserviceplan-jenkins"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  os_type             = "Linux"
+
+  sku_name = "B1"
+}
+
+resource "azurerm_linux_web_app" "this" {
+  name                = "terraformjenkins98742"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  service_plan_id     = azurerm_service_plan.this.id  # Updated attribute
+
+  site_config {
+    always_on = true
+  }
+}
